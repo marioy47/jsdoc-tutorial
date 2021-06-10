6d42b538e702713523a93a3541577279ec28285c
Mario Yepes <marioy47@gmail.com>
2021-06-10 09:15:28 -0500

    Adding a variables secition

---
+++ src/variables.js
@@ -0,0 +1,28 @@
+/**
+ * @type {String}
+ */
+var myStringVariable;
+
+/**
+ * This is the description of the variable
+ *
+ * @type {String[]}
+ */
+let myArrayVariable = [];
+
+/**
+ * This is a global variable
+ *
+ * @type {Array<Object>}
+ */
+
+const arrayOfObjects = [];
+
+arrayOfObjects.
+
+/**
+ * This is an array that can contain numbers or strings.
+ *
+ * @type {Array<number|string>}
+ */
+const arrayOfNumbersOrStrings = [];
